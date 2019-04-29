d90261e9f839dc500268010322c13703204ea7bc
Connelly R <42652898+ConnellyR@users.noreply.github.com>
2019-04-29 10:55:34 -0700

    Delete env.bat

--- templates/Fourm2/env.bat
+++
@@ -1,7 +0,0 @@
-set MONGO_USERNAME=MaddieComp
-set MONGO_PASSWORD=tjnYt8MPU8JTaRx5
-set MONGO_HOST=cluster0-v2sep.mongodb.net
-set MONGO_DBNAME=post
-set SECRET_KEY=qwertyuiopasdfghjkcvbnm
-set GITHUB_CLIENT_ID=b273f32002aaf4e923c8
-set GITHUB_CLIENT_SECRET=490e59ee53e8a47b20c948cc4c9bb78d9d3d343f
